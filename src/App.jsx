f918b65499767d1607c211e93bc7e392b58be403
jeyachandran17 <jeyachandran1733@gmail.com>
2023-03-28 05:35:15 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-
+      <Application />
     </div>
   )
 }
 
 export default App
+
+
+function Application() {
+  const navigate = useNavigate();
+  return (
+    <div className="Application">
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" onClick={()=>navigate('/')}>home</Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return(
+    <div className='home-page'>
+      <h3>welcome to start with captone project</h3>
+    </div>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div className="PageNotFound">
+      <h3>Page Not Found, check your correct url </h3>
+    </div>
+  );
+}
